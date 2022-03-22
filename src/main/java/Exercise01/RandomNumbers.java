959eeb1efe4f6dd44bf4c3345ffa5b60a3c064da
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2022-03-22 10:01:28 +0100

    exercise 1

--- src/main/java/Exercise01/RandomNumbers.java
+++ src/main/java/Exercise01/RandomNumbers.java
@@ -1,6 +1,7 @@
 package Exercise01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RandomNumbers {
@@ -8,18 +9,18 @@ public class RandomNumbers {
 
     public void go() {
         while (true) {
-            ArrayList<Integer> arrayOfRandomNumbers = getArrayOfRandomNumbers();
+            getArrayOfRandomNumbers();
         }
     }
 
-    private ArrayList<Integer> getArrayOfRandomNumbers() {
-        ArrayList<Integer> numbers = new ArrayList<>();
+    private int[] getArrayOfRandomNumbers() {
         System.out.println("how many numbers in this array? ");
         int length = sc.nextInt();
+        int[] numbers = new int[length];
         for (int i = 0; i < length; i++) {
-            numbers.add((int) (Math.random() * 100));
+            numbers[i] = (int) (Math.random() * 100);
         }
-        System.out.println("new array: "+ numbers);
+        System.out.println("new array: "+ Arrays.toString(numbers));
         return numbers;
     }
 
