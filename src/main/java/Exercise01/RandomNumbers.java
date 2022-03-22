511c29a87521b5ae9cbe3a7f0460f85b23215f90
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2022-03-22 10:27:01 +0100

    Remove printing

--- src/main/java/Exercise01/RandomNumbers.java
+++ src/main/java/Exercise01/RandomNumbers.java
@@ -20,7 +20,6 @@ public class RandomNumbers {
         for (int i = 0; i < length; i++) {
             numbers[i] = (int) (Math.random() * 100);
         }
-        System.out.println("new array: "+ Arrays.toString(numbers));
         return numbers;
     }
 
